35be898c6e569e01d16ebf4411a43fe9aa4ee6d8
Mikita Dzeviatau <9mikita@gmail.com>
2026-03-19 11:45:59 +0100

    chore: initialize spring repo

---
+++ src/main/java/com/every/expence/ExpenceApplication.java
@@ -0,0 +1,13 @@
+package com.every.expence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenceApplication.class, args);
+	}
+
+}
