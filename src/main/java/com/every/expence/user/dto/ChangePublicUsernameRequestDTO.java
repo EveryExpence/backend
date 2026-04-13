476c47c9a20cb1110be1f303a8920bb07dc16dd7
Mikita Dzeviatau <9mikita@gmail.com>
2026-04-13 18:23:08 +0200

    add custom message for username size

--- src/main/java/com/every/expence/user/dto/ChangePublicUsernameRequestDTO.java
+++ src/main/java/com/every/expence/user/dto/ChangePublicUsernameRequestDTO.java
@@ -4,6 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ChangePublicUsernameRequestDTO(
-                @NotBlank(message = "Username is required") @Size(min = 3, max = 30) String newPublicUsername) {
+        @NotBlank(message = "Username is required") @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters") String newPublicUsername) {
 
 }
