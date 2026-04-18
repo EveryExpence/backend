5f362005ee7add0fddb0c6d72eca3d76ff0d5395
lostbtw <bozo@>
2026-04-18 12:01:28 +0200

    add Account class

---
+++ src/main/java/com/every/expence/account/Account.java
@@ -0,0 +1,89 @@
+package com.every.expence.account;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "accounts")
+public class Account {
+    @Id
+    private String id;
+
+    @Indexed
+    private String ownerId;
+
+    private String name;
+
+    private String currency;
+
+    private BigDecimal balance;
+
+    private Instant createdAt;
+
+    public Account() {
+    }
+
+    public Account(String ownerId, String name, String currency, BigDecimal balance) {
+        this.ownerId = ownerId;
+        this.name = name;
+        this.currency = currency;
+        this.balance = balance;
+        this.createdAt = Instant.now();
+    }
+
+    public Account(String id, String ownerId, String name, String currency, BigDecimal balance) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.name = name;
+        this.currency = currency;
+        this.balance = balance;
+        this.createdAt = Instant.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
