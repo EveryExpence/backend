bf173dc2930a348b672ccff13f94d4ad10e8142f
lostbtw <bozo@>
2026-04-18 13:31:57 +0200

    fix allow account balance to be negative

--- src/main/java/com/every/expence/account/dto/CreateAccountRequestDTO.java
+++ src/main/java/com/every/expence/account/dto/CreateAccountRequestDTO.java
@@ -2,7 +2,6 @@ package com.every.expence.account.dto;
 
 import java.math.BigDecimal;
 
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -18,7 +17,6 @@ public record CreateAccountRequestDTO(
     String currency,
 
     @NotNull(message = "Balance is required")
-    @DecimalMin(value = "0.00", inclusive = true, message = "Balance must be non-negative")
     BigDecimal balance
 ) {
 }
