feb1b0ce1be0e9f549a13b7fcb7645ef9a608460
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-04-19 21:57:44 +0200

    add repository methods

--- src/main/java/com/every/expence/category/CategoryRepository.java
+++ src/main/java/com/every/expence/category/CategoryRepository.java
@@ -1,7 +1,11 @@
 package com.every.expence.category;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends MongoRepository<Category, String> {
-    
+    List<Category> findByUserId(String userId);
+    List<Category> findByUserIdAndType(String userId, String Type);
+    Optional<Category> findByUserIdAndNameAndType(String userId, String name, String type);
 }
