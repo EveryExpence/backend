1405c59e295825bf4ec68b16dbf9f6ff62216fa3
Mikita Dzeviatau <9mikita@gmail.com>
2026-04-21 01:08:34 +0200

    Refactor CreateRequestDTO

--- src/main/java/com/every/expence/expenseRecord/dto/CreateRequestDTO.java
+++ src/main/java/com/every/expence/expenseRecord/dto/CreateRequestDTO.java
@@ -5,11 +5,23 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
 public record CreateRequestDTO(
+        @NotNull(message = "Amount is required")
+        @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
         BigDecimal amount,
+
+        @NotNull(message = "Date is required")
         LocalDate date,
+
+        @NotNull(message = "Time is required")
         LocalTime time,
+
+        @NotNull(message = "Location is required")
         String location,
+
         String description,
         List<String> attachments) {
 
