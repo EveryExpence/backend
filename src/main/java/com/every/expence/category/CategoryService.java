939cc02f62343015ba068fe181f006000e8a52db
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-04-21 04:19:21 +0200

    add new delete/update methods to category service

--- src/main/java/com/every/expence/category/CategoryService.java
+++ src/main/java/com/every/expence/category/CategoryService.java
@@ -2,11 +2,14 @@ package com.every.expence.category;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 
 @Service
 public class CategoryService {
-    CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
@@ -28,6 +31,35 @@ public class CategoryService {
 
     public List<Category> getByUserId(String userId){
         return categoryRepository.findByUserId(userId);
+    }
+
+    public List<Category> getByUserIdAndType(String userId, String type){
+        return categoryRepository.findByUserIdAndType(userId, type);
+    }
+
+    public Category getById(String userId, String categoryId){
+        return categoryRepository.findByUserIdAndId(userId, categoryId).
+            orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+    }
+    
+
+    public Category updateCategory(String userId, String categoryId, String name, String type){
+        Category currCategory = getById(userId, categoryId);
+
+        if(categoryRepository.findByUserIdAndNameAndTypeAndId(userId, name, type, categoryId).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category already exists");
+        }
+
+        currCategory.setName(name);
+        currCategory.setUserId(name);
+        currCategory.setType(type);
+
+        return categoryRepository.save(currCategory);
+    }
+
+    public void deleteCategory(String userId, String categoryId, String name, String type){
+        Category currCategory = getById(userId, categoryId);
+        categoryRepository.delete(currCategory);
     }
 
     
